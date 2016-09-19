21161164c07c6160b960f09ba4adecf6d5778587
felix <felix@volny.co>
2016-09-19 14:33:17 +0700

    clickhandler, quarters light up

--- app/index.js
+++ app/index.js
@@ -23,11 +23,7 @@ function drawLines() {
   gb.stroke();
 }
 
-function drawBoard() {
-  // paint over board
-  gb.fillStyle = '#ffffff';
-  gb.fillRect(0, 0, width, width);
-
+function drawDisks() {
   gb.beginPath();
   gb.fillStyle = '#d7dde1';
   gb.arc(width / 2, width / 2, width / 2 - padding + lineWidth, 0, 2 * Math.PI);
@@ -37,7 +33,14 @@ function drawBoard() {
   gb.fillStyle = '#ecf0f1';
   gb.arc(width / 2, width / 2, width / 2 - padding, 0, 2 * Math.PI);
   gb.fill();
+}
 
+function drawBoard() {
+  // paint over board
+  gb.fillStyle = '#ffffff';
+  gb.fillRect(0, 0, width, width);
+
+  drawDisks();
   drawLines();
 }
 
@@ -50,8 +53,7 @@ function drawQuarter(start, end, color) {
   gb.fill();
 }
 
-function colorButton(coordinates) {
-  let color;
+function activateQuarter(coordinates) {
   switch (coordinates) {
     case '00':
       drawQuarter(Math.PI, Math.PI * (3/2), '#2ecc71');
@@ -70,9 +72,31 @@ function colorButton(coordinates) {
   drawLines();
 }
 
+function handleClick(e) {
+  const canvasWidth = canvas.offsetWidth;
+  const x = e.pageX - canvas.offsetLeft;
+  const y = e.pageY - canvas.offsetTop;
+
+  if (x < width / 2 && y < width /2) {
+    activateQuarter('00');
+    window.setTimeout(drawBoard, 500);
+  } else if (x < width / 2 && y > width / 2) {
+    activateQuarter('01');
+    window.setTimeout(drawBoard, 500);
+  } else if (x > width / 2 && y < width / 2) {
+    activateQuarter('10');
+    window.setTimeout(drawBoard, 500);
+  } else if (x > width / 2 && y > width / 2) {
+    activateQuarter('11');
+    window.setTimeout(drawBoard, 500);
+  }
+}
+
+canvas.addEventListener('click', handleClick, false);
+
 drawBoard();
-//colorButton('00');
-//colorButton('10');
-//colorButton('01');
-//colorButton('11');
+//activateQuarter('00');
+//activateQuarter('10');
+//activateQuarter('01');
+//activateQuarter('11');
 
