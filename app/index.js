81510cb9a2122569a59345eb755a2e7acd6c42a6
felix <felix@volny.co>
2016-09-20 19:16:57 +0700

    working turn-based game

--- app/index.js
+++ app/index.js
@@ -1,74 +1,73 @@
 import './style.scss';
 
-const canvas = document.querySelector('#gameboard');
-const gb = canvas.getContext('2d');
-
-// CANVAS SIZES
-const width = 500;
-const padding = 30;
-const lineWidth = 15;
-
-const backgroundColor = '#ecf0f1';
-const lineColor = '#d7dde1';
+const canvas = {
+  dom: document.querySelector('#gameboard'),
+  context: document.querySelector('#gameboard').getContext('2d'),
+  width: 500,
+  padding: 30,
+  lineWidth: 15,
+  backgroundColor: '#ecf0f1',
+  lineColor: '#d7dde1'
+}
 
 function drawLines() {
-  gb.lineWidth = lineWidth;
-  gb.strokeStyle = lineColor;
+  canvas.context.lineWidth = canvas.lineWidth;
+  canvas.context.strokeStyle = canvas.lineColor;
 
-  gb.beginPath();
-  gb.moveTo(width / 2, padding);
-  gb.lineTo(width / 2, width - padding);
-  gb.stroke();
+  canvas.context.beginPath();
+  canvas.context.moveTo(canvas.width / 2, canvas.padding);
+  canvas.context.lineTo(canvas.width / 2, canvas.width - canvas.padding);
+  canvas.context.stroke();
 
-  gb.beginPath();
-  gb.moveTo(padding, width / 2);
-  gb.lineTo(width - padding, width / 2);
-  gb.stroke();
+  canvas.context.beginPath();
+  canvas.context.moveTo(canvas.padding, canvas.width / 2);
+  canvas.context.lineTo(canvas.width - canvas.padding, canvas.width / 2);
+  canvas.context.stroke();
 }
 
 function drawDisks() {
-  gb.beginPath();
-  gb.fillStyle = lineColor;
-  gb.arc(width / 2, width / 2, width / 2 - padding + lineWidth, 0, 2 * Math.PI);
-  gb.fill();
+  canvas.context.beginPath();
+  canvas.context.fillStyle = canvas.lineColor;
+  canvas.context.arc(canvas.width / 2, canvas.width / 2, canvas.width / 2 - canvas.padding + canvas.lineWidth, 0, 2 * Math.PI);
+  canvas.context.fill();
 
-  gb.beginPath();
-  gb.fillStyle = backgroundColor;
-  gb.arc(width / 2, width / 2, width / 2 - padding, 0, 2 * Math.PI);
-  gb.fill();
+  canvas.context.beginPath();
+  canvas.context.fillStyle = canvas.backgroundColor;
+  canvas.context.arc(canvas.width / 2, canvas.width / 2, canvas.width / 2 - canvas.padding, 0, 2 * Math.PI);
+  canvas.context.fill();
 }
 
 function drawBoard() {
   // paint over board
-  gb.fillStyle = '#ffffff';
-  gb.fillRect(0, 0, width, width);
+  canvas.context.fillStyle = '#ffffff';
+  canvas.context.fillRect(0, 0, canvas.width, canvas.width);
 
   drawDisks();
   drawLines();
 }
 
 function drawQuarter(start, end, color) {
-  gb.beginPath();
-  gb.arc(width / 2, width / 2, (width / 2) - padding, start, end);
-  gb.lineTo(width / 2, width / 2);
-  gb.closePath();
-  gb.fillStyle = color;
-  gb.fill();
+  canvas.context.beginPath();
+  canvas.context.arc(canvas.width / 2, canvas.width / 2, (canvas.width / 2) - canvas.padding, start, end);
+  canvas.context.lineTo(canvas.width / 2, canvas.width / 2);
+  canvas.context.closePath();
+  canvas.context.fillStyle = color;
+  canvas.context.fill();
 }
 
 function activateQuarter(coordinates, deactivate) {
   switch (coordinates) {
     case '00':
-      drawQuarter(Math.PI, Math.PI * (3/2), deactivate ? backgroundColor : '#2ecc71');
+      drawQuarter(Math.PI, Math.PI * (3/2), deactivate ? canvas.backgroundColor : '#2ecc71');
       break;
     case '10':
-      drawQuarter(Math.PI * (3/2), Math.PI * 2, deactivate ? backgroundColor : '#e74c3c');
+      drawQuarter(Math.PI * (3/2), Math.PI * 2, deactivate ? canvas.backgroundColor : '#e74c3c');
       break;
     case '01':
-      drawQuarter(Math.PI / 2, Math.PI, deactivate ? backgroundColor : '#3498db');
+      drawQuarter(Math.PI / 2, Math.PI, deactivate ? canvas.backgroundColor : '#3498db');
       break;
     case '11':
-      drawQuarter(0, Math.PI / 2, deactivate ? backgroundColor : '#f1c40f');
+      drawQuarter(0, Math.PI / 2, deactivate ? canvas.backgroundColor : '#f1c40f');
       break;
   }
 
@@ -76,71 +75,69 @@ function activateQuarter(coordinates, deactivate) {
 }
 
 let inputs = [];
-const sequence = ['10', '00'];//, '00', '01', '10', '11', '00', '01'];
+const sequence = []; //['10', '00'];//, '00', '01', '10', '11', '00', '01'];
+
+function addToSequence() {
+  const coordinates = ['00', '01', '10', '11'];
+  sequence.push(coordinates[Math.floor(Math.random() * coordinates.length)]);
+}
+
+function gameOver() {
+  canvas.dom.removeEventListener('click', handleClick, false);
+  showMessage('Game Over');
+  window.setTimeout(() => {
+    showMessage('Highscore: ' + parseInt(sequence.length - 1).toString(), false);
+    document.querySelector('#restart').style.visibility = 'visible';
+  }, 700);
+}
 
 function checkClick() {
   console.debug('sequence:', sequence[inputs.length -1], 'input:', inputs[inputs.length -1]);
 
   if (sequence[inputs.length -1] === inputs[inputs.length -1]) {
-    console.log('correct input')
+    if (sequence.length === inputs.length) {
+      showMessage('Good Job!');
+      window.setTimeout(() => {
+        computersTurn(sequence);
+      }, 700)
+    }
   } else {
-    console.log('wrong input')
-    showMessage('Game Over');
-    window.setTimeout(() => {
-      showMessage('Highscore: ' + parseInt(inputs.length - 1).toString(), false);
-    }, 700)
+    gameOver();
   }
-
 }
 
 function handleClick(e) {
-  const canvasWidth = canvas.offsetWidth;
-  const x = e.pageX - canvas.offsetLeft;
-  const y = e.pageY - canvas.offsetTop;
+  const canvasWidth = canvas.dom.offsetWidth;
+  const x = e.pageX - canvas.dom.offsetLeft;
+  const y = e.pageY - canvas.dom.offsetTop;
 
-  if (x < width / 2 && y < width /2) {
+  if (x < canvas.width / 2 && y < canvas.width /2) {
     activateQuarter('00');
     window.setTimeout(() => activateQuarter('00', true), 500);
     inputs.push('00');
-  } else if (x < width / 2 && y > width / 2) {
+  } else if (x < canvas.width / 2 && y > canvas.width / 2) {
     activateQuarter('01');
     window.setTimeout(() => activateQuarter('01', true), 500);
     inputs.push('01');
-  } else if (x > width / 2 && y < width / 2) {
+  } else if (x > canvas.width / 2 && y < canvas.width / 2) {
     activateQuarter('10');
     window.setTimeout(() => activateQuarter('10', true), 500);
     inputs.push('10');
-  } else if (x > width / 2 && y > width / 2) {
+  } else if (x > canvas.width / 2 && y > canvas.width / 2) {
     activateQuarter('11');
     window.setTimeout(() => activateQuarter('11', true), 500);
     inputs.push('11');
   }
 
   checkClick();
-
 }
-
-canvas.addEventListener('click', handleClick, false);
-document.querySelector('#restart').addEventListener('click', () => {
-  // remove gameover message
-  if (document.querySelector('.message')) {
-    document.body.removeChild(document.querySelector('.message'));
-  }
-
-  drawBoard();
-
-  computersTurn(sequence);
-}, false)
-
-drawBoard();
-
 
 function showMessage(string, destroy = true) {
   const message = document.createElement("p");
   message.className += "message animated zoomIn";
   const text = document.createTextNode(string);
   message.appendChild(text);
-  document.body.insertBefore(message, canvas);
+  document.body.insertBefore(message, canvas.dom);
   if (destroy) {
     window.setTimeout(() => {
       document.body.removeChild(message);
@@ -165,13 +162,33 @@ function playSequence(coords, callback) {
 function playersTurn() {
   showMessage('Your turn');
   inputs = [];
+  // enable input
+  canvas.dom.addEventListener('click', handleClick, false);
 }
 
 function computersTurn(coords) {
+  addToSequence();
+  canvas.dom.removeEventListener('click', handleClick, false);
   showMessage('Watch closely');
-
   window.setTimeout(() => {
     playSequence(coords, playersTurn);
   }, 900)
 }
+
+function restart() {
+  if (document.querySelector('.message')) {
+    document.body.removeChild(document.querySelector('.message'));
+  }
+  drawBoard();
+  computersTurn(sequence);
+
+  this.textContent = 'Restart';
+  this.style.visibility = 'hidden';
+}
+
+document.querySelector('#restart').addEventListener('click', restart, false)
+
+window.onload = function() {
+  drawBoard();
+};
 
